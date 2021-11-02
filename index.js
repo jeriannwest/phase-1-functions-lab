1dc9c3909b01f985bfccec935a1dbf49689eaf4c
+ <jeriannwest@yahoo.com>
2021-11-02 06:46:08 -0400

    commit tree 3eae616815eacebedc9785cf5c4a9e355829b858

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    return Math.abs(blocks - 42)
+}
+function distanceFromHqInFeet(blocks){
+    return distanceFromHqInBlocks(blocks)*264
+}
+function distanceTravelledInFeet(start,end){
+    return Math.abs(start - end)*264 
+}
+function calculatesFarePrice(start,end){
+    let feetDistance = distanceTravelledInFeet(start , end)
+    if (feetDistance < 400){
+    return 0
+    } else if(feetDistance >= 400 && feetDistance <= 2000){
+    return ((feetDistance - 400) * .02)
+    } else if (feetDistance > 2000 && feetDistance<2500){
+    return 25
+    } else {
+    return 'cannot travel that far'
+    }
+}
